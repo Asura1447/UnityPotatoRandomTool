6cc7c283f5dcf12968a84c434ae985dbce80a8a5
FuryPotato <Asura1447@gmail.com>
2020-02-28 15:03:26 +0800

    初次上传
    简单的道具随机启用，包括单个的和随机组的

---
+++ RandomActive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomActive : MonoBehaviour
+{
+    public int 启用概率;//预制体的启用概率 
+    public void 启用判断() {
+        int 随机生成数 = Random.Range(0, 101);   //随机生成一个整数 范围在0到101之间包括0不包括101
+        if (随机生成数 <= 启用概率){             //比较两个数的大小，若低于启用概率则启用该GameObject
+            this.gameObject.SetActive(true);
+        } else {
+            this.gameObject.SetActive(false);    //高于启用概率则禁用该GameObject
+        }
+    }
+}
---
+++ RandomActiveGroup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomActiveGroup : MonoBehaviour
+{
+    public GameObject[] 预制组;             //这个预制组包涵了一组需要的生成的所有备选预制
+    public void 启用预制() {
+        int 预制组长度 = 预制组.Length;     //数一下这个预制组的长度
+        禁用全员(预制组长度);                         //先将所有的成员禁用,防止该方法被调用多次，导致启用多个预制
+        int 随机数 = Random.Range(0, 预制组长度);   //在此上限为预制组长度-1，因为数组的索引是0，如果+1的话就会超出长度从而报错
+        预制组[随机数].gameObject.SetActive(true);  //设置启用一个预制  
+    }
+    void 禁用全员(int 预制组长度) {   
+        for (int i = 0; i < 预制组长度;i++) {   //遍历数组，并将所有成员禁用
+            预制组[i].SetActive(false);
+        }
+    }
+}
